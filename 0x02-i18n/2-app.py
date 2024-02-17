d1b9426c2a64192a95ee2861e3c37402b0d77ec8
besufikad <07yilmabese@gmail.com>
2024-02-17 12:41:14 +0300

    debug and edit

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,18 +2,18 @@
 '''
     Use Babel to get user locale.
 '''
-
 from flask_babel import Babel
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder="templates")
 babel = Babel(app)
 
 
-class Config(object):
+class Config:
     '''
-        Babel configuration.
+    The `Config` class defines language settings and default locale
+    and timezone for a Python application.
     '''
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -24,20 +24,24 @@ app.config.from_object(Config)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def helloWorld() -> str:
-    '''
-        Render template for Babel usage.
-    '''
+def helloworld() -> str:
+    """
+    The function `helloworld` returns the rendered
+    template '1-index.html'.
+    """
     return render_template('2-index.html')
 
 
 @babel.localeselector
-def get_locale() -> str:
-    '''
-        Get user locale.
-    '''
+def get_locale():
+    """
+    The function `get_locale()` returns the best matching
+    language based on the accepted languages and
+    the configured languages in the application.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+# babel.init_app(app, locale_selector=get_locale)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
