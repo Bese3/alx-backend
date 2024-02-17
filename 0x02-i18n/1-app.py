caedf16052051920d46e1add8e9388388e2b53c2
besufikad <07yilmabese@gmail.com>
2024-02-17 11:40:43 +0300

    debug and edit

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,17 +2,18 @@
 '''
     Basic Babel setup.
 '''
-
 from flask_babel import Babel
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__, template_folder='templates')
+
+app = Flask(__name__, template_folder="templates")
 babel = Babel(app)
 
 
-class Config(object):
+class Config:
     '''
-        Babel configuration.
+    The `Config` class defines language settings and default locale
+    and timezone for a Python application.
     '''
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -23,12 +24,13 @@ app.config.from_object(Config)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def helloWorld() -> str:
-    '''
-        Render template for Babel usage.
-    '''
+def helloworld() -> str:
+    """
+    The function `helloworld` returns the rendered
+    template '1-index.html'.
+    """
     return render_template('1-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
